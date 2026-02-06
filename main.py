642b34e5befc4f3ff1ca2ee3d43d03c3d6e9d366
brunorosa-votu <bruno.f.rosa6@aluno.senai.br>
2026-02-06 10:54:01 -0300

    Botão 2.0

--- main.py
+++ main.py
@@ -9,34 +9,37 @@ botao = Pin (35, Pin.IN, Pin.PULL_UP)
 
 tempo = 0
 contador = 0
-pausa = 1
+pausa = False
 
 def apagar():
     led1.off()
     led2.off()
     led3.off()
+apagar()
+i = True
 
-i = 0
+def espera():
+    time.sleep(0.1)
 
 while True:
     tempo = 0
     inicio = time.time()
 
-    if botao.value() == 1 and i == 0:
-        i = 1
+    if botao.value() == 1 and i == True:
+        i = False
         while tempo < 2:
             fim = time.time()
             tempo = fim - inicio
 
-            if botao.value() == 1 and pausa == 1:
+            if botao.value() == 1 and pausa == False:
                 contador += 1
-                pausa = 0
+                pausa = True
 
             if botao.value() == 0:
-                pausa = 1
+                pausa = False
 
-            time.sleep(0.1)
-    i = 0
+            espera()
+    i = True
 
 
     if contador == 1:
@@ -56,15 +59,15 @@ while True:
         while s == True:
             apagar()
             led3.on()
-            time.sleep(0.1)
+            espera()
             apagar()
-            time.sleep(0.1)
+            espera()
             led2.on()
-            time.sleep(0.1)
+            espera()
             apagar()
-            time.sleep(0.1)
+            espera()
             led1.on()
-            time.sleep(0.1)
+            espera()
             if botao.value() == 1:
                 s = False
 
